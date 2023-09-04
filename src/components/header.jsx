10ef4b77c689cb832904eb5eb6dece3e2ab043e2
Amarjit Singh Arora <asarora72@gmail.com>
2023-09-04 15:11:12 +0530

    updated gigignore

---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1 style={{color: "#FFF8DC"}}>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p style={{color: "#483D8B", fontWeight: "bold"}}>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
