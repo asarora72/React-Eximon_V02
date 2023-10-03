e1842f2ffafe6dcd82043e465592bbcc7a9a67ab
Amarjit Singh Arora <asarora72@gmail.com>
2023-10-03 14:13:39 +0530

    Header compo para made bold

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,7 +12,7 @@ export const Header = (props) => {
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p><b>{props.data ? props.data.paragraph : "Loading"}</b></p>
                 <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
